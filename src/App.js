12bbdde70c5bbd6f0685327fc4081caa1638bd6f
neykhant <khantmaw48@gmail.com>
2022-02-18 15:00:13 +0630

    add react route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Pages/Home';
+import Contact from './components/Pages/Contact';
+import Service from './components/Pages/Service';
+import About from './components/Pages/About';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+       <Router>
+           <Navbar/>
+           <Routes>
+           <Route exact path="/" element={<Home/>}/>
+           <Route path="/contact" element={<Contact/>}/>
+           <Route path="/service" element={<Service/>}/>
+           <Route path="/about" element={<About/>}/>
+           </Routes>
+       </Router>
     </div>
   );
 }
---
+++ src/components/Navbar/MenuList.js
@@ -0,0 +1,18 @@
+export const MenuList = [
+    {
+        title: "Home",
+        url: "/"
+    },
+    {
+        title: "About",
+        url: "/about"
+    },
+    {
+        title: "Contact",
+        url: "/contact"
+    },
+    {
+        title: "Services",
+        url: "/service"
+    }
+]
